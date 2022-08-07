9c22ca7419cee89d845e0c2e9d137e65eeae1deb
PurplikDev <vojta.g@atlas.cz>
2022-08-07 22:51:41 +0200

    funny

--- js/script.js
+++ js/script.js
@@ -11,20 +11,3 @@ profile_picture.addEventListener('mouseout', () => {
       profile_picture.src = "images/purplik.png";
 });
 */
-
-const lib = require('lib')({token: process.env.STDLIB_SECRET_TOKEN});
-
-await lib.discord.channels['@0.3.0'].messages.create({
-  "channel_id": `${context.params.event.channel_id}`,
-  "content": "",
-  "tts": false,
-  "embeds": [
-    {
-      "type": "rich",
-      "title": `Definitely not an IP Grabber`,
-      "description": `I swear`,
-      "color": 0x7307bb,
-      "url": `https://purplikdev.github.io`
-    }
-  ]
-});
