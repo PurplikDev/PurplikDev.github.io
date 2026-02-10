fa66697514a421799741c1901a253a3726877e04
PurplikDev <vojta.g@atlas.cz>
2026-02-10 18:04:54 +0100

    header improvements

--- js/main.js
+++ js/main.js
@@ -9,3 +9,14 @@ const observer = new IntersectionObserver((entries) => {
 
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((_element) => observer.observe(_element));
+
+
+
+const projects = document.querySelectorAll('.currentProject');
+for(let i = 0; i < projects.length; i++) {
+      if(i % 2 == 1) {
+            projects[i].classList.add('left')
+      } else {
+            projects[i].classList.add('right')
+      }
+}
